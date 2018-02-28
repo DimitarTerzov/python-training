55c18d4b437b9852f8b3b41ce495a5c909170b79
DimitarTerzov <dimiterat@gmail.com>
2018-02-28 14:12:36 +0200

    Add another example.

--- use_of_map.py
+++ use_of_map.py
@@ -1,2 +1,3 @@
 kingdoms = ['Bacteria', 'Protozoa', 'Chromista', 'Plantae', 'Fungi', 'Animalia']for kingdom in map(kingdoms.__getitem__, [0,3]):
     print(kingdom)
+print(list(map(kingdoms.__getitem__, [0, 1, 3])))
