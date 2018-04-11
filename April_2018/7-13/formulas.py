4f444ad9d0ac4a0f61d5d9627c27117f5972a8c1
NNiklenov <nikolai.niklenov@gmail.com>
2018-04-11 18:03:05 +0300

    Repetition_Based_on_User_Input

---
+++ April_2018/7-13/formulas.py
@@ -0,0 +1,14 @@
+text = ""
+while text != "quit":
+    text = input("Please enter a chemical formula (or 'quit' to exit): ")
+    if text == "quit":
+        print("...exiting program")
+    elif text == "H2O":
+        print("Water")
+    elif text == "NH3":
+        print("Amonia")
+    elif text == "CH4":
+        print("Methane")
+    else:
+        print("Unknown compound")
+        
