f987c9a430843508d791e7729ad7fb5ee75eae87
NNiklenov <nikolai.niklenov@gmail.com>
2018-04-22 17:46:52 +0300

    new_Files

---
+++ April_2018/14-20/rat_2.py
@@ -0,0 +1,9 @@
+rat_1 = [18123, 1321, 3213, 213]
+rat_2 = [19223, 423, 32423, 923]
+
+if rat_1[0] > rat_2[0] and rat_1[-1] > rat_2[-1]:
+    print("Rat 1 remained heavier than rat 2.")
+else:
+    print("Rat 2 became heavier than rat 1.")
+    
+    
---
+++ April_2018/14-20/sum1.py
@@ -0,0 +1,10 @@
+sum1 = 0
+count = 0
+for number in range(2, 23):
+    sum1 += number
+    count += 1
+    
+    average = sum1 / count
+    
+    print(average)
+    
