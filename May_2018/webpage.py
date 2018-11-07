2a5db5358c066cb5c6c70b70581136acdc14eca2
NNiklenov <36667728+NNiklenov@users.noreply.github.com>
2018-11-07 12:22:04 +0200

    Delete webpage.py

--- May_2018/webpage.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-url = 'http://robjhyndman.com/tsdldata/ecologyl/hopedale.dat'
-with urllib.request.urlopen(url) as webpage:
-    for line in webpage:
-        line = line.strip()
-        line = line.decode('utf-8')
-        print(line)
-        
