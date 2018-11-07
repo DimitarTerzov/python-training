5c6bd48f45fad2014d99a13eaded4bba0d281540
NNiklenov <36667728+NNiklenov@users.noreply.github.com>
2018-11-07 12:28:26 +0200

    Delete count_values_11.8_04.py

--- Jun/count_values_11.8_04.py
+++
@@ -1,9 +0,0 @@
-def count_values(dictionary):
-    """ (dict) -> int
-    
-    Return the number of unique values in dictionary.
-    
-    >>> count_values({'humans': 5, 'planets': 8, 'balls: 5'})
-    2
-    """
-    return len(set(dictionary.values()))
