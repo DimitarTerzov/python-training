a979810f0b04e19cdc208c24279e00b7fda891ce
NNiklenov <36667728+NNiklenov@users.noreply.github.com>
2018-11-07 12:24:51 +0200

    Delete lists_elements.py

--- May_2018/Exercises_10.10/lists_elements.py
+++
@@ -1,8 +0,0 @@
-with open('alkaline_metals.txt', 'r') as elements_file:
-    elements = elements_file.readlines()
-    
-    elements = [x.strip() for x in elements]
-
-    
-
-    print([[i] for i in elements])
