b10e5035b0bd58724def413a6c8fbb8968cb02d3
NNiklenov <36667728+NNiklenov@users.noreply.github.com>
2018-11-07 12:07:33 +0200

    Delete use_of_map.py

--- use_of_map.py
+++
@@ -1,3 +0,0 @@
-kingdoms = ['Bacteria', 'Protozoa', 'Chromista', 'Plantae', 'Fungi', 'Animalia']for kingdom in map(kingdoms.__getitem__, [0,3]):
-    print(kingdom)
-print(list(map(kingdoms.__getitem__, [0, 1, 3])))
